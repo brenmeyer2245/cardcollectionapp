4a6243a006a158b45809a88d3cf86a670c41dfa9
Brendan Meyer <bmeyer@RP-BMeyer.local>
2020-07-10 09:09:46 -0500

    Added sass loaders Added sass files to client and pointed web config at whole client Added Routes Added History Added React index Added Article component

--- client/index.js
+++ client/index.js
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom';
-import React from 'react'
-import App from './app'
+import React from 'react';
+import App from './app';
 import "./scss/main.scss";
+import {Router} from 'react-router-dom';
+import history from './history';
 
 ReactDOM.render(
-  <App />,
+  <Router history={history}>
+     <App />
+  </Router>,
   document.getElementById("app")
 );
