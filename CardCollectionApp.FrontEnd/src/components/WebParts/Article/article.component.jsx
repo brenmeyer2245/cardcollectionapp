344838c6fe10c0611e3d993c1af30131850e5273
brenmeyer2245 <brenmeyer2245@gmail.com>
2021-09-25 09:11:07 -0600

    Update Articles

--- CardCollectionApp.FrontEnd/src/components/WebParts/Article/article.component.jsx
+++ CardCollectionApp.FrontEnd/src/components/WebParts/Article/article.component.jsx
@@ -3,25 +3,27 @@ import React from 'react';
 const Article = (props) => {
   return (
       (
-      <div className="WebPart-Container">
+      <div className="WebPart-container">
           {/* Label */}
-          <h1 className="WebPart-Title"> {props.vm.title} </h1>
+          <h1 className="WebPart-title"> {props.vm.title} </h1>
 
           {/* Text is broken down into an array of chunks */}
           {props.vm.contentText.map(
             (textChunk, index) => {
               console.log(textChunk, index)
               return (
-                <div key={index}>
-                  <p>
+                <div key={index} className="Article-section">
+                  <p className="Article-text">
                     {textChunk}
                   </p>
                   {/* if there's an image at this index, include it */}
                   {
                     props.vm.contentImages[index] &&
                     (
-                    <figure>
-                      <img
+                    <>
+                    <div className="clearfix"></div> 
+                    <figure className={`Article-figure ${index % 2 ? "float-right" : "float-left"}`} > 
+                      <img className="Article-figureImage"
                         alt = { props.vm.contentImages[index].label }
 
                         src = {props.vm.contentImages[index].src}
@@ -31,12 +33,14 @@ const Article = (props) => {
                             determineFloatByIndex(index)
                           }
                       />
-                      <figcaption>
+                      <figcaption className="Article-figureCaption">
                         { props.vm.contentImages[index].label  }
                       </figcaption>
                     </figure>
+                   </>
                     )
                   }
+
                 </div>
               )
             })
